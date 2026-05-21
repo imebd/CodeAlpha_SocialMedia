23ebc91660eacb8f455ccb0e2ffb03acf4363a02
imebd <imanelbridi5@email.com>
2026-05-21 17:15:31 +0200

    structure de base

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+    res.send("L'API du réseau  est en ligne !");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré avec succès sur le port ${PORT}`);
+});
